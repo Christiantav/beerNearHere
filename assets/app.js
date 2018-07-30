262c49428d8e2beff4df76ad136c20a71f042729
s34 <s34@katherine-anguss-macbook-pro.local>
2018-07-30 19:50:38 -0400

    updated js files

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,11 @@
 // Here are my changes
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyASUkrBX5ae-DENzm3fL3arKwlRCrD1qBA",
+    authDomain: "beernearhere-2363f.firebaseapp.com",
+    databaseURL: "https://beernearhere-2363f.firebaseio.com",
+    projectId: "beernearhere-2363f",
+    storageBucket: "beernearhere-2363f.appspot.com",
+    messagingSenderId: "857706302849"
+  };
+  firebase.initializeApp(config);
